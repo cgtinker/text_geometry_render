7b59100e5d9b4038a6488024e5b95fc21ac9ab60
dhsu <d.hsu@online.de>
2021-12-15 00:52:14 +0100

    slight changes

--- Assets/Scripts/AppendText.cs
+++ Assets/Scripts/AppendText.cs
@@ -1,67 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class AppendText : MonoBehaviour
 {
-	private TextMeshProUGUI textObject;
-	private string text;
+	private TextMeshProUGUI m_textObject;
 	System.Random random = new System.Random();
 
 	private void Awake()
 	{
-		textObject = this.gameObject.GetComponent<TextMeshProUGUI>();
-		text = "";
+		m_textObject = this.gameObject.GetComponent<TextMeshProUGUI>();
 	}
 
-
-	public string RandomString(int length)
+	private string RandomString(int length)
 	{
-		const string chars = "01010*  ";
+		const string chars = "001 ";
 		return new string(Enumerable.Repeat(chars, length)
 			.Select(s => s[random.Next(s.Length)]).ToArray());
 	}
 
-	private IEnumerator AssignChar(char letter)
+	private void AssignCharacter(char c)
 	{
-		text += letter;
-		textObject.text = text;
-		yield return new WaitForEndOfFrame();
+		m_textObject.text += c;
+	}
 
-		if (text.Count() > 6500)
+	private void FixedUpdate()
+	{
+		List<string> strs = new List<string>();
+		strs.Add(RandomString(Random.Range(0, 25)));
+		strs.Add(RandomString(Random.Range(0, 125)));
+		strs.Add(RandomString(Random.Range(0, 400)));
+
+
+		foreach (char c in strs[Random.Range(0, strs.Count)])
 		{
-			text = "";
+			AssignCharacter(c);
+		}
+
+		if (m_textObject.text.Length > 6000)
+		{
+			m_textObject.text = "";
 		}
 	}
-
-	private void AssignMsg(string msg)
-	{
-		textObject.text = text + msg;
-	}
-
-	private void Update()
-	{
-		int rand = Random.Range(0, 10);
-		if (rand == 1)
-		{
-			int length = Random.Range(0, 500);
-			string msg = RandomString(length);
-
-			foreach (char entry in "$" + msg)
-			{
-				StartCoroutine(AssignChar(entry));
-			}
-
-			string str = "#";
-			for (int i = 0; i < rand; i++)
-			{
-				str += "#";
-			}
-			AssignMsg(str);
-		}
-	}
-
-
 }
